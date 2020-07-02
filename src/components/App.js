b6fe317fbbd633c38845c32571428b449524fde3
PolThm <pol.thomas56@gmail.com>
2020-07-02 18:34:44 +0200

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import HeroArea from "./HeroArea";
 
 const App = () => {
--- src/sass/batFiles/scssWatch.bat
+++ src/sass/batFiles/scssWatch.bat
@@ -1,1 +1,1 @@
-sass src/sass/style.scss src/App.css --watch
+sass src/sass/style.scss src/components/App.css --watch
