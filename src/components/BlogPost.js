9f4f8bf3a555fad5e5f233ac9e93faacb7c688d4
PolThm <pol.thomas56@gmail.com>
2020-07-06 20:42:34 +0200

    General update

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -4,7 +4,11 @@ import {Container} from "react-bootstrap";
 const BlogPost = () => {
   return (
     <Container>
-
+      <div className={"title"}>
+        <h2>Latest news of trim</h2>
+        <h5>All the Lorem Ipsum generators on the Internet tend to repeat <br/>
+          predefined chunks as necessary making this the first</h5>
+      </div>
     </Container>
   )
 };
